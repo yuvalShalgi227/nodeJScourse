1d17897c6232f78478515c988ad3d4518d990ed4
Yuval Shalgi <yuvals@perfectomobile.com>
2022-02-09 20:33:41 +0200

    memo where i understand it

--- factorail.js
+++ factorail.js
@@ -3,19 +3,9 @@ const isErrorInInput = require('./checkInput')
 const args = process.argv;
 if (isErrorInInput(args)) return;
 const num = args[2];
+///--------------------
 
 function factorail(num) {
-    if (num === 0 || num === 1) return 1;
-    let res =1;
-    let temp = num;
-    for (i=0; i<temp; i++) {
-        res *= num;
-        num = num -1; 
-    }
-    return res;
-}
-
-function factorail2(num) {
     if (num <2 ) return 1;
     res =1;
     let temp = num;
@@ -26,11 +16,23 @@ function factorail2(num) {
     return res;
 }
 
-function factorailMemo(num) {
+function factorailRecuresive(num) {
     if (num < 2) return 1;
     while (num > 0) {
         return num *= factorailMemo(num -1);
     }
 }
 
-console.log(factorailMemo(num))
+function factorailMemo(cash) {
+    return (n =cash) => { 
+        while (n > 0) {
+            if (n < 2) return 1;
+            return n *= x(n-1);
+        }
+    }
+}
+
+
+const x = factorailMemo(num);
+
+console.log(x());
